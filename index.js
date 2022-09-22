efd6bb3c27958ac9cdb98b4541e53118fb252966
RamS24 <AlSaadi.Rami24@gmail.com>
2022-09-22 16:11:02 +0200

    solution

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ document.body.addEventListener('click', e => {
 })
 window.addEventListener('mousemove', e => {
     document.querySelector('main').innerText = '';
-    document.querySelector('main').append("pageX: ", e.pageX,
-        "pageY: ", e.pageY,
-        "clientX: ", e.clientX,
-        "clientY:", e.clientY, window.innerHeight, window.innerWidth, '\n',
-        (e.clientX / window.innerWidth).toFixed(2), '\n', (e.clientY / window.innerHeight).toFixed(2));
+    document.querySelector('main').innerText += ("pageX: " + e.pageX +
+        "pageY: " + e.pageY +
+        "clientX: " + e.clientX +
+        "clientY:" + e.clientY + window.innerHeight + window.innerWidth + '\n' +
+        (e.clientX / window.innerWidth).toFixed(2) + '\n' + (e.clientY / window.innerHeight).toFixed(2));
     if (e.clientY < 10)
-    // if (e.clientX / window.innerWidth > 0.20 && e.clientY / window.innerHeight > 0.27 && e.clientX / window.innerWidth < 0.84 && e.clientY / window.innerHeight < 0.7)
         document.querySelector('.popup').style = 'display: block; opacity:1;';
 })
+
+document.querySelector('form').addEventListener('submit', (e) => e.preventDefault())
